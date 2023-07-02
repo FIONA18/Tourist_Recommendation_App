d9ff5ddd6515905b278c4f4e98e2a2ebfa4a9ecb
Fiona Lishal <32681518+FIONA18@users.noreply.github.com>
2023-07-02 13:26:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./styles.css";
+import React, { useState } from "react";
+import touristsDictionary from "./touristsDictionary";
+
+export default function App() {
+  var categories = ["Beaches", "Water parks", "Hill Stations"];
+
+  var [touristPlaces, getTouristPlaces] = useState([]);
+
+  function OnCategoryClicked(category) {
+    getTouristPlaces(touristsDictionary[category]);
+  }
+  return (
+    <div className="App">
+      <h1>Hey Tourists!!!</h1>
+      <h2 class="Subheading">
+        Tourism promotes intercultural understanding and fosters connections
+        between people from different backgrounds. So, check out some of the
+        tourist places you would like to visit
+      </h2>
+      <ul class="Category">
+        {categories.map((category) => (
+          <li class="CategoryItem" onClick={() => OnCategoryClicked(category)}>
+            <div>{category}</div>
+          </li>
+        ))}
+      </ul>
+      <ul class="TouristPlaces">
+        {touristPlaces.map((touristPlace) => (
+          <li class="TouristPlace">{touristPlace}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/touristsDictionary.js
@@ -0,0 +1,21 @@
+var touristsDictionary = {
+  Beaches: [
+    "Palolem, South Goa",
+    "Gokarna Beach, Karnataka",
+    "Baga, North Goa"
+  ],
+  "Water parks": [
+    "GRS Fantasy Park, Mysore",
+    "Wonderla, Bangalore",
+    "Oysters, Gurgaon",
+    "Funcity, Chandigarh"
+  ],
+  "Hill Stations": [
+    "Srinagar, Jammu & Kashmir",
+    "Manali, Himachal Pradesh",
+    "Ooty, Tamil Nadu",
+    "Munnar, Kerala",
+    "Coorg, Karnataka"
+  ]
+};
+export default touristsDictionary;
